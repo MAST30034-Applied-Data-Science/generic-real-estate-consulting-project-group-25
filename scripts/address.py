a7a5b7622f7fb364df8306b399590f4b92b26572
Ohan Ra <ohanr@Pavillion.localdomain>
2022-09-19 12:44:10 +1000

    route

--- scripts/address.py
+++ scripts/address.py
@@ -1,34 +1,48 @@
-#Converitng address/name of location strings to long and lat
-def addressToCords(address: str):
-    from geopy.geocoders import Nominatim
+from geopy.geocoders import Nominatim
+import requests
+import re
+
+#Converting address/name of location strings to long and lat
+def addressToCords(address: str):    
     geolocator = Nominatim(user_agent="addToCords")
     location = geolocator.geocode(address)
+    if location is None:
+        try:
+            # Getting rid of unit number
+            address = re.sub("^.+\/", "", address)
+            location = geolocator.geocode(address)
+            return [location.latitude, location.longitude]
+        except:
+            # Only using one block as a reference 
+            try:
+                address = re.sub("[0-9]+\-", "", address)
+                location = geolocator.geocode(address)
+                return [location.latitude, location.longitude]
+            except AttributeError:
+                return "Requires Manual Inspection"        
+
     return [location.latitude, location.longitude]
+ 
 
-#Find distance between two places with geocords    
-def dist_between_two_lat_lon(*args):
-    from math import asin, cos, radians, sin, sqrt
-    lat1, lat2, long1, long2 = map(radians, args)
 
-    dist_lats = abs(lat2 - lat1) 
-    dist_longs = abs(long2 - long1) 
-    a = sin(dist_lats/2)**2 + cos(lat1) * cos(lat2) * sin(dist_longs/2)**2
-    c = asin(sqrt(a)) * 2
-    radius_earth = 6378 # the "Earth radius" R varies from 6356.752 km at the poles to 6378.137 km at the equator.
-    return c * radius_earth
+#Find driving distance between two places 
+# This code requires an OpenRouteService backend running on localhost
+# Refer to: https://giscience.github.io/openrouteservice/
+def dist_between_two_lat_lon(lat1, lat2, long1, long2, method = "driving-car"):
+    response = requests.get(f"http://localhost:8080/ors/v2/directions/{method}?&start={long1},{lat1}&end={long2},{lat2}")
+    return response.json()["features"][0]["properties"]["segments"][0]["distance"]
 
 #Get the closest location to a given house in rent dataset. 
 #Provided that external data and rent data already have geocord as column
-def find_closest_location(rent_row, ext):
-    rent_cord = rent_row["geocord"]
-    ext_cord = ext["geocord"]
-    return min(ext_cord, key=lambda x: dist_between_two_lat_lon(rent_cord[0], x[0], rent_cord[1], ext[1]))
+def find_closest_location(rent_cord, ext, ext_cord_col):
+    ext_cord = ext[ext_cord_col]    
+    return min(ext_cord, key=lambda x: dist_between_two_lat_lon(rent_cord[0], x[0], rent_cord[1], x[1]))
 
 
-def assign_closest_location(rent, ext, ext_name):
+def assign_closest_location(rent, ext, ext_name, rent_cor_col, ext_cor_col):
     import numpy as np
-    func = lambda x: find_closest_location(x, ext)
-    rent_geo = rent["geoloc"].to_numpy()
+    func = lambda x: find_closest_location(x, ext, rent_cor_col, ext_cor_col)
+    rent_geo = rent[[rent_cor_col]].to_numpy()
     vfunc = np.vectorize(func)
     rent_geo = vfunc(rent_geo)
     rent[f'closest_{ext_name}'] = rent_geo.tolist()
