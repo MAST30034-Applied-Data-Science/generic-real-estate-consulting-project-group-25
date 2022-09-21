5d8fd0e00850bfee625ee9116d31bc4cfc0188c5
Ohan Ra <ohanr@Pavillion.localdomain>
2022-09-21 11:13:52 +1000

    dst to cbd

--- scripts/nearbyFeatures.py
+++ scripts/nearbyFeatures.py
@@ -1,4 +1,5 @@
 import requests
+from address import dist_between_two_lat_lon
 
 def nearbyFeatures(lat, lng, radius, type: str, amenity:str):
     overpass_url = "http://overpass-api.de/api/interpreter"
@@ -15,11 +16,12 @@ def nearbyFeatures(lat, lng, radius, type: str, amenity:str):
 
     return data
 
-def getAll(type:str, amenity:str, box = "-39.192401, 141.028939, -34.005138, 151.077642"):
+def getAllStation(box = "-39.192401, 141.028939, -34.005138, 151.077642"):
     overpass_url = "http://overpass-api.de/api/interpreter"
     overpass_query = f"""
     [out:json];
-    (node["{type}"="{amenity}"]({box});
+    (node["railway"="station"]({box});
+    node["railway"="stop"]({box});
     );
     out;
     """
@@ -28,3 +30,27 @@ def getAll(type:str, amenity:str, box = "-39.192401, 141.028939, -34.005138, 151
     data = response.json()
 
     return data
+
+def getNearestStation(lat_lst, lng_lst, lat, lng, radius = 1000):
+    inc = 1000
+    overpass_url = "http://overpass-api.de/api/interpreter"
+    overpass_query = f"""
+    [out:json];
+    (node(around:{radius},{lat},{lng})["railway"="station"];
+    (node(around:{radius},{lat},{lng})["railway"="stop"]
+    );
+    out;
+    """
+    response = requests.get(overpass_url, 
+                            params={'data': overpass_query})
+    data = response.json()
+    if data["elements"] == []:
+        return getAllStation(lat, lng, radius+inc)
+    else:
+        n = len(data["elements"])
+        dst = []
+        for i in range(0, n):
+            lat_st = data["elements"][i]["lat"]
+            lng_st = data["elements"][i]["lon"]
+            dst.append(dist_between_two_lat_lon(lat_st, lat_lst, lng_st, lng_lst))
+        return min(dst)
