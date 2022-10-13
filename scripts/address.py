14c957e8c67c27e3ed8feea20d077092a05a160a
ohanr <ohan.ra@arup.com>
2022-10-13 19:34:24 +1100

    added comments

--- scripts/address.py
+++ scripts/address.py
@@ -1,54 +1,60 @@
-from ast import Index
-from time import time
-from geopy.geocoders import Nominatim
 import requests
 import re
-from geopy.exc import GeocoderUnavailable
-import json
 
-#Converting address/name of location strings to long and lat
 def addressToCords(address: str):  
+    """
+    Converting address/name of location strings to long and lat
+    """
     # Getting rid of unit number and only using one block as reference to help with search
     address = re.sub("^.+\/", "", address)
     address = re.sub("[0-9]+\-", "", address) 
-    geolocator = Nominatim(user_agent="addToCords")
-    
+    query = re.sub(" ", "+", address)
+    query = re.sub(",", "%2C", query)
+    url = f"https://nominatim.openstreetmap.org/search?q={query}&format=json&limit=1"   
     try:
-        location = geolocator.geocode(address, timeout=5)
-        return location.latitude, location.longitude
-    except GeocoderUnavailable:
+        location = requests.get(url).json()
+        return location[0]["lat"], location[0]["lon"] 
+    # If there is no matching result, get rid of some non-unique words as sometimes they don't match with the database
+    except IndexError:
         try:
-            query = re.sub(" ", "+", address)
-            query = re.sub(",", "%2C", query)
+            query = re.sub("Drive||Street||Avenue||Road||Court||Crescent", "", query)
             url = f"https://nominatim.openstreetmap.org/search?q={query}&format=json&limit=1"   
             location = requests.get(url).json()
-            return location[0]["lat"], location[0]["lon"]  
+            return location[0]["lat"], location[0]["lon"] 
+        # If the address still cannot be found by hand, it needs to be retrieved manually
         except IndexError:
             return "Requires Manual Inspection"
     except AttributeError:   
         return "Requires Manual Inspection"
 
 def find_min_dist(lat, lng, ext_data, ext_lat_col, ext_lng_col, ext_name_col, method = "driving-car"):
-    MAX_ROUTE = 100
-    distances = []
+    """
+    Find the closest driving distance between a starting coordinate and a list of destination coordinates
+    This code requires an local OpenRouteService core service running simultaneously 
+    Refer to: https://giscience.github.io/openrouteservice/ fpor instructions on installation and usuage
+
+    lat: latitude of starting coordinate
+    lng: longitude of starting coordinate 
+    ext_data: a pandas dataframe containing the destinations and their geocordinates
+    ext_lat_col: column name of ext_data that stores latitude
+    ext_lng_col: column name of ext_data that stores longitude 
+    ext_name_col: column name of ext_data that stores the name of the destination
+    """ 
+
     sources = [[lng, lat]]
-    for n in range(1, int(len(ext_data)/MAX_ROUTE)+1):
-        end = n*MAX_ROUTE
-        if n*MAX_ROUTE > len(ext_data):
-            end = len(ext_data)
-        for i in range(0, end):
-            lng_ext = ext_data[ext_lng_col][i] 
-            lat_ext = ext_data[ext_lat_col][i]
-            sources.append([lng_ext, lat_ext])        
-        data = {
-                    "locations":sources,
-                    "destinations":[0],
-                    "metrics":["distance"],
-                    "sources":[x for x in range(1, len(sources))]
-                }
-        response = requests.post(f"http://localhost:8080/ors/v2/matrix/{method}", json = data)
-        result = response.json()["distances"]
-        distances += result
+    # Getting the distance into the required format
+    for i in range(0, len(ext_data)):
+        lng_ext = ext_data[ext_lng_col][i] 
+        lat_ext = ext_data[ext_lat_col][i]
+        sources.append([lng_ext, lat_ext])        
+    data = {
+                "locations":sources,
+                "destinations":[0],
+                "metrics":["distance"],
+                "sources":[x for x in range(1, len(sources))]
+            }
+    response = requests.post(f"http://localhost:8080/ors/v2/matrix/{method}", json = data)
+    result = response.json()["distances"]
     
     dst = min(result)
     loc = ext_data[ext_name_col][result.index(dst)]
@@ -56,33 +62,12 @@ def find_min_dist(lat, lng, ext_data, ext_lat_col, ext_lng_col, ext_name_col, me
     return dst, loc
 
 
-#Get the closest location to a given house in rent dataset. 
-#Provided that external data and rent data already have geocord as column
-def find_closest_location(rent_lat, rent_long, ext_data, ext_lat_col, ext_lon_col, name_col):
-    ext_data["cords"] = ext_data[[ext_lat_col, ext_lon_col, name_col]].values.tolist()
-    closest = min(ext_data["cords"], key=lambda x: dist_between_two_lat_lon(rent_lat, x[0], rent_long, x[1]))
-    return dist_between_two_lat_lon(rent_lat, closest[0], rent_long, closest[1]), closest[2]
 
 
 
-#Find driving distance between two places 
-# This code requires an OpenRouteService backend running on localhost
-# Refer to: https://giscience.github.io/openrouteservice/
-def dist_between_two_lat_lon(lat1, lat2, long1, long2, method = "driving-car"):
-    response = requests.get(f"http://localhost:8080/ors/v2/directions/{method}?&start={long1},{lat1}&end={long2},{lat2}")
-    try:
-        return response.json()["features"][0]["properties"]["segments"][0]["distance"]
-    except KeyError:
-        return 100000
+
+
         
-def assign_closest_location(rent, ext, ext_name, rent_cor_col, ext_cor_col):
-    import numpy as np
-    func = lambda x: find_closest_location(x, ext, rent_cor_col, ext_cor_col)
-    rent_geo = rent[[rent_cor_col]].to_numpy()
-    vfunc = np.vectorize(func)
-    rent_geo = vfunc(rent_geo)
-    rent[f'closest_{ext_name}'] = rent_geo.tolist()
-    return rent
     
 
 
--- scripts/nearbyFeatures.py
+++ scripts/nearbyFeatures.py
@@ -1,22 +1,12 @@
 import requests
-from address import dist_between_two_lat_lon
 
-def nearbyFeatures(lat, lng, radius, type: str, amenity:str):
-    overpass_url = "http://overpass-api.de/api/interpreter"
-    overpass_query = f"""
-    [out:json];
-    (node(around:{radius},{lat},{lng})
-    ["{type}"="{amenity}"];
-    );
-    out;
+def getAllFeatures(key:str, value:str, box = "-39.192401, 141.028939, -34.005138, 151.077642"):
     """
-    response = requests.get(overpass_url, 
-                            params={'data': overpass_query})
-    data = response.json()
-
-    return data
-
-def getAllFeatures(key, value, box = "-39.192401, 141.028939, -34.005138, 151.077642"):
+    Gets all features of a specific key category within a rentangular bounding box.
+    (The default bounding box includes whole of Victoria and partial NSW)
+    
+    key, value: plug in values based on: https://wiki.openstreetmap.org/wiki/Key:amenity
+    """
     overpass_url = "http://overpass-api.de/api/interpreter"
     overpass_query = f"""
     [out:json];
@@ -28,27 +18,3 @@ def getAllFeatures(key, value, box = "-39.192401, 141.028939, -34.005138, 151.07
                             params={'data': overpass_query})
     data = response.json()
     return data
-
-def getNearestStation(lat_lst, lng_lst, lat, lng, radius = 1000):
-    inc = 1000
-    overpass_url = "http://overpass-api.de/api/interpreter"
-    overpass_query = f"""
-    [out:json];
-    (node(around:{radius},{lat},{lng})["railway"="station"];
-    (node(around:{radius},{lat},{lng})["railway"="stop"];
-    );
-    out;
-    """
-    response = requests.get(overpass_url, 
-                            params={'data': overpass_query})
-    data = response.json()
-    if data["elements"] == []:
-        return getAllStation(lat, lng, radius+inc)
-    else:
-        n = len(data["elements"])
-        dst = []
-        for i in range(0, n):
-            lat_st = data["elements"][i]["lat"]
-            lng_st = data["elements"][i]["lon"]
-            dst.append(dist_between_two_lat_lon(lat_st, lat_lst, lng_st, lng_lst))
-        return min(dst)
